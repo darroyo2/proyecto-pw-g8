f883c5d7c27e4da7da57cd6b43489e17b6e77d07
kiyoshi713 <112112870+kiyoshi713@users.noreply.github.com>
2022-10-03 12:21:04 -0500

    c

--- src/pages/Ranking2Page.jsx
+++ src/pages/Ranking2Page.jsx
@@ -16,80 +16,12 @@ const RankingsPage = () => {
 	
     return (
         <body class="b">
-	<h1 class="h">Ranking List</h1>
+	<h1 class="h">Ranking Perifericos</h1>
 	<div class="contentlist">
+		
+		
 		<div class="layerlist">
-			<h2 class="g">01</h2>
-			<div class="pcparts">
-				<h4>Tarjeta grafica</h4>
-				<h5></h5>
-			</div>
-			<img class="i" src={i5} alt="imageAlbum"/>
-			<div class="optionslist">
-				<img class="d" src= {i1}alt="download"/>
-				<img class="d" src= {i2}alt="heart"/>
-				<img class="d" src= {i3}alt="share"/>
-				<img class="d" src= {i4}alt="dots"/>
-			</div>
-		</div>
-		<div class="layerlist">
-			<h2>02</h2>
-			<div class="pcparts">
-				<h4>Core i5</h4>
-				<h5></h5>
-			</div>
-			<img class="i" src={i6} alt="imageAlbum"/>
-			<div class="optionslist">
-				<img class="d" src={i1} alt="download"/>
-				<img class="d" src={i2} alt="heart"/>
-				<img class="d" src={i3} alt="share"/>
-				<img class="d" src={i4} alt="dots"/>
-			</div>
-		</div>
-		<div class="layerlist">
-			<h2>03</h2>
-			<div class="pcparts">
-				<h4>Memoria ram</h4>
-				<h5></h5>
-			</div>
-			<img class="i" src={i7} alt="imageAlbum"/>
-			<div class="optionslist">
-				<img class="d" src={i1} alt="download"/>
-				<img class="d" src={i2} alt="heart"/>
-				<img class="d" src={i3} alt="share"/>
-				<img class="d" src={i4} alt="dots"/>
-			</div>
-		</div>
-		<div class="layerlist">
-			<h2>04</h2>
-			<div class="pcparts">
-				<h4>Pantalla</h4>
-				<h5></h5>
-			</div>
-			<img class="i" src={i8} alt="imageAlbum"/>
-			<div class="optionslist">
-				<img class="d" src={i1} alt="download"/>
-				<img class="d" src={i2} alt="heart"/>
-				<img class="d" src={i3} alt="share"/>
-				<img class="d" src={i4} alt="dots"/>
-			</div>
-		</div>
-		<div class="layerlist">
-			<h2>05</h2>
-			<div class="pcparts">
-				<h4>CPU BOX</h4>
-				<h5></h5>
-			</div>
-			<img class="i" src={i9} alt="imageAlbum"/>
-			<div class="optionslist">
-				<img class="d" src={i1} alt="download"/>
-				<img class="d" src={i2} alt="heart"/>
-				<img class="d" src={i3} alt="share"/>
-				<img class="d" src={i4}alt="dots"/>
-			</div>
-		</div>
-		<div class="layerlist">
-			<h2>06</h2>
+			<h2>01</h2>
 			<div class="pcparts">
 				<h4>Ventilador PC</h4>
 				<h5></h5>
@@ -103,7 +35,7 @@ const RankingsPage = () => {
 			</div>
 		</div>
 		<div class="layerlist">
-			<h2>07</h2>
+			<h2>02</h2>
 			<div class="pcparts">
 				<h4>Camara web</h4>
 				<h5></h5>
@@ -117,7 +49,7 @@ const RankingsPage = () => {
 			</div>
 		</div>
 		<div class="layerlist">
-			<h2>08</h2>
+			<h2>03</h2>
 			<div class="pcparts">
 				<h4>Mouse gamer</h4>
 				<h5></h5>
