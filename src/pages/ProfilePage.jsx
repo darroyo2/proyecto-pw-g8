f4109c08339b97914cdae06982b9f59d444b9929
darroyo2 <20202370@aloe.ulima.edu.pe>
2022-12-01 06:38:50 -0500

    Update Profile Hecho

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,44 @@
 import "../styles/ProfilePage.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const ProfilePage = () => {
+  const [correo, setCorreo] = useState("")
+  const [nombre, setNombre] = useState("")
+  const [telefono, setTelefono] = useState("")
+  const [apellido, setApellido] = useState("")
+  const [zip, setZip] = useState("")
+  const [departamento, setDepartamento] = useState("")
+  const [ciudad, setCiudad] = useState("")
+  const [direccion, setDireccion] = useState("")
+  const httpGuardarPerfil = async (usuario, nombre, apellido, correo, direccion, departamento, ciudad, zip, telefono) => {
+    const data = {
+      usuario_id :  usuario,
+      perfil_nombre : nombre,
+      perfil_apellido : apellido,
+      perfil_correo : correo,
+      perfil_direccion : direccion,
+      perfil_Departamento : departamento,
+      perfil_Ciudad : ciudad,
+      perfil_Zip : zip,
+      perfil_Telefono : telefono,
+        
+    }
+    const resp = await fetch("http://localhost:4444/perfil", {
+        method : "POST",
+        body : JSON.stringify(data),
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    })
+    const dataResp = await resp.json()
+    if (dataResp.error !== "") {
+        console.log("Hubo un error")
+    }
+}
+const savePerfil = (nombre, apellido, correo, direccion, departamento, ciudad, zip, telefono) => {
+    console.log( `nombre : ${nombre} apellido :  ${apellido} correo : ${correo} direccion : ${direccion} departamento : ${departamento}`)
+    httpGuardarPerfil("222571e6-05a8-4bec-9caa-a6dd5bfeecfc", nombre, apellido, correo, direccion, departamento, ciudad, zip, telefono)
+}
   const navigate = useNavigate();
   return (
     <body className="fondo">
@@ -101,20 +139,20 @@ const ProfilePage = () => {
               <span className="form-label inline last">Last Name</span>
               <div></div>
               <span>
-                <input className="form-first form-control" value="Ed" />
+                <input className="form-first form-control" type = "text" value ={nombre} onChange={(evt) => { setNombre(evt.target.value)} }/>
               </span>
               <span>
-                <input className="form-last form-control" value="Va"/>
+                <input className="form-last form-control" type = "text" value ={apellido} onChange={(evt) => { setApellido(evt.target.value)} }/>
               </span>
               <div></div>
               <label className="form-label">Email</label>
-              <input className="form-control" />
+              <input className="form-control" type = "text" value ={correo} onChange={(evt) => { setCorreo(evt.target.value)} }/>
               <label className="form-label">Address</label>
-              <input className="form-control" />
+              <input className="form-control" type = "text" value ={direccion} onChange={(evt) => { setDireccion(evt.target.value)} } />
               <span className="form-label">Aparmaent, suit, etc</span>
               <span className="form-label inline city">City</span>
-              <input className="form-first form-control" />
-              <input className="form-last form-control" />
+              <input className="form-first form-control" type = "text" value ={departamento} onChange={(evt) => { setDepartamento(evt.target.value)} }/>
+              <input className="form-last form-control" type = "text" value ={ciudad} onChange={(evt) => { setCiudad(evt.target.value)} } />
               <label className="form-label">Country/Region</label>
               <label className="form-label zip">Postal/zip code</label>
               <div></div>
@@ -130,11 +168,11 @@ const ProfilePage = () => {
                 <option>Japan</option>
                 <option>Spain</option>
               </select>
-              <input className="form-last form-control" />
+              <input className="form-last form-control" type = "text" value ={zip} onChange={(evt) => { setZip(evt.target.value)} }/>
               <label className="form-label">Phone</label>
-              <input className="form-control" />
+              <input className="form-control" type = "text" value ={telefono} onChange={(evt) => { setTelefono(evt.target.value)} }/>
               <div></div>
-              <button className="btn update btn-primary btn-lg">
+              <button className="btn update btn-primary btn-lg" onClick={() => {savePerfil (nombre, apellido, correo, direccion, departamento, ciudad, zip, telefono)}}>
                 Update Info
               </button>
               <span className="cancel">Cancel</span>
